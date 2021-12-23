667ba11bd128283ca97d33b1f4b9a24914a1640e
Yevhen Malyshko <memx86@gmail.com>
2021-12-23 22:37:36 +0200

    хедер, мобильное меню

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,21 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileLink = document.querySelectorAll('.mobile-menu__link');
 
-  menuBtnRef.addEventListener("click", () => {
+  const doMagic = () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-    mobileMenuRef.classList.toggle("is-open");
-    document.body.classList.toggle("modal-open");
-  });
+    mobileMenuRef.classList.toggle('is-open');
+    document.body.classList.toggle('modal-open');
+  };
+
+  menuBtnRef.addEventListener('click', doMagic);
+  for (let i = 0; i < mobileLink.length; i++) {
+    mobileLink[i].addEventListener('click', doMagic);
+  }
 })();
